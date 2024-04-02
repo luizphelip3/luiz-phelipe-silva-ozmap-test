77e22c6d8f46380e07605b0ca177cb5ece7870d1
luizphelip3 <luiz_phelip3@hotmail.com>
2024-04-02 11:30:17 -0300

    refactor: removed unused function

--- src/modules/shared/utils/functions/remove-undefined-params.ts
+++ src/modules/shared/utils/functions/remove-undefined-params.ts
@@ -1,14 +1,3 @@
-export function removeUndefinedParams(params: Object): any {
-  const result: any = {};
-
-  Object.keys(params).forEach((key) => {
-    if (params[key] !== undefined) {
-      result[key] = params[key];
-    }
-  });
-  return result;
-}
-
 export function removeNullAndUndefinedParams(params: Object): any {
   const result: any = {};
 
